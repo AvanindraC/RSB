a71d9961884abc7c3702461dc1c6ae77f6fa4927
Avanindra Chakraborty <77975448+AvanindraC@users.noreply.github.com>
2021-06-07 19:32:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 apt update
-sudo apt install python3.9
+sudo apt install python3.8
 
 sudo apt install python3-pip 
 sudo apt install wget
